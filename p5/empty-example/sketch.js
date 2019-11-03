498759b88b32c08ff08369e6e1197060c56e418b
emanuelebuda <33718148+emanuelebuda@users.noreply.github.com>
2019-11-03 12:39:58 +0100

    Update sketch.js

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,28 +1,7 @@
-float x, y;
-float dim = 80.0;
+function setup() {
 
-void setup() {
-  size(640, 360);
-  noStroke();
 }
 
-void draw() {
-  background(102);
-  
-  x = x + 0.8;
-  
-  if (x > width + dim) {
-    x = -dim;
-  } 
-  
-  translate(x, height/2-dim/2);
-  fill(255);
-  rect(-dim/2, -dim/2, dim, dim);
-  
-  // Transforms accumulate. Notice how this rect moves 
-  // twice as fast as the other, but it has the same 
-  // parameter for the x-axis value
-  translate(x, dim);
-  fill(0);
-  rect(-dim/2, -dim/2, dim, dim);
+function draw() {
+  ellipse(50, 50, 80, 80);
 }
